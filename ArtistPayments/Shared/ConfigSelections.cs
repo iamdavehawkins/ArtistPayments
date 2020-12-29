9a2e9300faa99373f5612bf66eedd368b38cb1f1
David Hawkins <iamdavehawkins@gmail.com>
2020-12-28 21:00:31 -0700

    initial commit

---
+++ ArtistPayments/Shared/ConfigSelections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ArtistPayments.Shared
+{
+    public class ConfigSelections
+    {
+        public Decimal payPerStream = 0.004M;
+        public Decimal _monthlySubscriptionCost = 9.99M;
+        public string monthlySubscriptionCost
+        {
+            get
+            {
+                return _monthlySubscriptionCost.ToString();
+            }
+            set
+            {
+                _monthlySubscriptionCost = Decimal.Parse(value);
+            }
+        }
+    }
+}
---
+++ ArtistPayments/Shared/Listen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ArtistPayments.Shared
+{
+    public class Listen
+    {
+        public string EndTime { get; set; }
+        public string ArtistName { get; set; }
+        public string TrackName { get; set; }
+        public int MsPlayed { get; set; }
+        public DateTime EndDateTimeUTC
+        {
+            get
+            {
+                return DateTime.Parse(EndTime);
+            }
+        }
+        public int Month
+        {
+            get
+            {
+                return DateTime.Parse(EndTime).Month;
+            }
+        }
+    }
+}
